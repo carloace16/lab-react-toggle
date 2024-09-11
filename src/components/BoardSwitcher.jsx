32c879deaf5853ec468621d94839b6295a0c6cc6
Carlo Ace Sagad <carloace166@gmail.com>
2024-09-11 19:53:15 -0400

    fixed the code

--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +11,24 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  // Use state to track the currently selected board
+  const [selectedBoard, setSelectedBoard] = useState(0);
+
+  // Function to handle toggle button click
+  const handleToggle = () => {
+    setSelectedBoard((prevBoard) => (prevBoard + 1) % props.numBoards);
+  };
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedBoard; // Updated to use state for selected board
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={handleToggle}>Toggle</button> {/* Add onClick to toggle button */}
     </div>
   );
 }
